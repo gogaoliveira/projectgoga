dfe6cf8bd3d3d4f2f8f28e1faf87c5d161fc97d5
Goga <2doug077@gmail.com>
2021-04-30 11:49:26 -0300

    started project

---
+++ src/main/java/douglasoliveiraprojectgoga/ProjectgogaApplication.java
@@ -0,0 +1,13 @@
+package douglasoliveiraprojectgoga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectgogaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectgogaApplication.class, args);
+	}
+
+}
